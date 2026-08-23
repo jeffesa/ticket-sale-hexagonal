b17fcb4fea587662928ce623033fac366e690e1b
Jefferson Santos <jeffesa@gmail.com>
2026-08-22 21:29:12 -0300

    feat: setup inicial Spring Boot 4.1.1 com Java 17

---
+++ src/main/java/br/com/ticketsale/TicketSaleHexagonalApplication.java
@@ -0,0 +1,13 @@
+package br.com.ticketsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSaleHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSaleHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ticketsale/TicketSaleHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ticketsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSaleHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
